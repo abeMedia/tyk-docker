b2b30dadaf122e564f2eae19f725a708cbe9022f
Adam Bouqdib <adam@abemedia.co.uk>
2016-10-27 03:15:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tyk API Gateway
+
+Tyk is a lightweight, open source API Gateway and Management Platform enables you to control who accesses your API.
+See [tyk.io](https://tyk.io/) for more information about Tyk.
+
+This image is targeted at simple setups where you only have a single (unversioned) API to manage.
---
+++ run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e
+
+cp .docker/tyk.conf ./tyk.conf
+cp .docker/app.json ./apps/app.json
+
+# set tyk config
+sed -i "s/TYK_SECRET/$TYK_SECRET/g" ./tyk.conf
+sed -i "s/REDIS_HOST/$REDIS_HOST/g" ./tyk.conf
+sed -i "s/REDIS_PORT/$REDIS_PORT/g" ./tyk.conf
+sed -i "s/REDIS_PASSWORD/$REDIS_PASSWORD/g" ./tyk.conf
+
+# set api definition
+sed -i "s/API_NAME/$API_NAME/g" ./apps/app.json
+sed -i "s/API_ID/$API_ID/g" ./apps/app.json
+sed -i "s/ORG_ID/$ORG_ID/g" ./apps/app.json
+sed -i "s/USE_KEYLESS/$USE_KEYLESS/g" ./apps/app.json
+sed -i "s/AUTH_HEADER_NAME/$AUTH_HEADER_NAME/g" ./apps/app.json
+sed -i "s/AUTH_USE_PARAM/$AUTH_USE_PARAM/g" ./apps/app.json
+sed -i "s#TARGET_URL#$TARGET_URL#g" ./apps/app.json
+sed -i "s/ENABLE_BATCH_REQUESTS/$ENABLE_BATCH_REQUESTS/g" ./apps/app.json
+
+# set policies
+POLICY_TEMPLATE=$(<.docker/policies.json)
+POLICY_TEMPLATE="${POLICY_TEMPLATE//API_NAME/$API_NAME}"
+POLICY_TEMPLATE="${POLICY_TEMPLATE//API_ID/$API_ID}"
+POLICY_TEMPLATE="${POLICY_TEMPLATE//ORG_ID/$ORG_ID}"
+
+function getVar()
+{
+    local var="$1"
+    echo "${!var}"
+}
+
+POLICIES="{
+"
+i="1"
+POLICY_ID="$POLICY_1_ID";
+while [[ ! -z "$POLICY_ID" ]]; do
+  POLICY_RATE=$(getVar POLICY_${i}_RATE)
+  POLICY_PER=$(getVar POLICY_${i}_PER)
+  POLICY_QUOTA_MAX=$(getVar POLICY_${i}_QUOTA_MAX)
+  POLICY_QUOTA_RENEWAL_RATE=$(getVar POLICY_${i}_QUOTA_RENEWAL_RATE)
+
+  POLICY="${POLICY_TEMPLATE//POLICY_ID/$POLICY_ID}"
+  POLICY="${POLICY//POLICY_RATE/$POLICY_RATE}"
+  POLICY="${POLICY//POLICY_PER/$POLICY_PER}"
+  POLICY="${POLICY//POLICY_QUOTA_MAX/$POLICY_QUOTA_MAX}"
+  POLICY="${POLICY//POLICY_QUOTA_RENEWAL_RATE/$POLICY_QUOTA_RENEWAL_RATE}"
+
+  POLICIES+="$POLICY"
+  ((i++))
+
+  POLICY_ID=$(getVar POLICY_${i}_ID);
+  if [ ! -z "$POLICY_ID" ]; then
+    POLICIES+=",
+"
+  fi
+done
+
+POLICIES+="
+}"
+
+echo "$POLICIES" > ./policies/policies.json
+
+tyk --conf ./tyk.conf
